61b3d542aba30b96047d9b2b20b7d2a0c5abb922
PiotrBosak <PiotrBosak@yahoo.com>
2020-08-01 17:58:51 +0200

    init

---
+++ src/main/java/pb/chess/webchessappservice/demo/ChesswebappApplication.java
@@ -0,0 +1,13 @@
+package pb.chess.webchessappservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesswebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChesswebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pb/chess/webchessappservice/demo/ChesswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.chess.webchessappservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesswebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
